04fb4fbf47c15699a4341af63aa902d29a7b8d32
Tori <mes.viktoria@mail.ru>
2022-09-10 05:02:22 +0500

    Cколько чисел больше 0 ввёл пользователь

---
+++ Task041/Program.cs
@@ -0,0 +1,18 @@
+﻿// сколько чисел больше 0 ввёл пользователь
+Console.WriteLine("Введите количество цифр: ");
+int n = int.Parse (Console.ReadLine());
+int [] number = new int[n];
+int i = 0;
+for(int j = 1; j <= n; j++)
+{
+    Console.WriteLine($"Введите число {j}: ");
+    number[i] = int.Parse (Console.ReadLine());
+    i++;
+} 
+
+int count = 0;
+for(i = 0; i < n; i++)
+{
+    if(number[i] > 0) count++;
+}
+Console.WriteLine(count);
